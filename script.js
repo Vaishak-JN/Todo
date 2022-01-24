c27d70cac871f01864e6df99a05ab5bbe8edd283
Vaishak-JN <vaishakjnair.98@gmail.com>
2022-01-24 17:58:10 +0530

    Added dark/light mod toggle

--- script.js
+++ script.js
@@ -4,19 +4,35 @@ const input = document.getElementById("create")
 const hidden = document.getElementById("hidden")
 const todoList = document.getElementById("list")
 const pending = document.getElementById("pending")
+const themeBtn = document.getElementById("theme")
+
+let themeMode = true
+
+function changeTheme() {
+    themeMode = !themeMode
+    themeBtn.innerHTML = themeMode ? `<i class="bi bi-moon h4 "></i>` : `<i class="bi bi-brightness-high h4 "></i>`
+    // document.body.style.backgroundColor = themeMode ? "white" : "rgba(0, 0, 0, 0.825)"
+    document.getElementById("todo").style.backgroundColor = themeMode ? "white" : "black"
+    document.body.className = themeMode ? "light" : "dark"
+
+    add.className = themeMode ? "btn btn-outline-dark" : "btn btn-outline-info"
+
+    display()
+}
+
 
 let list = JSON.parse(localStorage.getItem("list")) || []
 
 function display() {
     let display = ""
     list.forEach(ele => {
-        display = display + `<div class="me-1 row justify-content-between align-items-center">
-                                <button class="col-1 btn" onclick="taskDone(${ele.id})"><i class="bi bi-check2 text-success h4"></i></button>
-                                <h4 class="${ele.done ? "text-decoration-line-through lead col-9" : "col-9"}" id="${ele.id}">${ele.item}</h4>
-                                <button class="col-1 btn" id="edit" onclick="editItem(${ele.id})"><i class="bi bi-pencil text-info h5"></i></button>
-                                <button class="col-1 btn" id="delete" onclick="deleteItem(${ele.id})"><i class="bi bi-x text-danger h3"></i></button>
-                            </div>
-                            `
+        display = display +
+            `<div class="me-1 row justify-content-between align-items-center">
+                <button class="col-1 btn" onclick="taskDone(${ele.id})"><i class="bi bi-check2 text-success h4"></i></button>
+                <h4 class="${ele.done ? "text-decoration-line-through lead col-9" : "col-9"} ${themeMode ? "text-dark" : "text-light"}" id="${ele.id}">${ele.item}</h4>
+                <button class="col-1 btn" id="edit" onclick="editItem(${ele.id})"><i class="bi bi-pencil text-info h6"></i></button>
+                <button class="col-1 btn" id="delete" onclick="deleteItem(${ele.id})"><i class="bi bi-x text-danger h3"></i></button>
+            </div>`
     })
     todoList.innerHTML = display
     pendingTasks()
@@ -95,14 +111,12 @@ function taskDone(id) {
     })
     list = [...pendingList]
     localStorage.setItem('list', JSON.stringify(list));
-
-
     display()
 }
 
 function pendingTasks() {
     let pendingList = list.filter(ele => ele.done === false)
-    pending.innerHTML = list.length === 0 ? "" : `<h4>You have <span class="text-danger">${pendingList.length}</span> pending tasks</h4>`
+    pending.innerHTML = list.length === 0 ? "" : `<h5 class=${themeMode ? "text-dark" : "text-light"}>You have <span class=${themeMode ? "text-danger" : "text-info"}>${pendingList.length}</span> pending tasks</h5>`
 }
 
 
